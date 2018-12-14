be795eca7d62437f14dbb0538bf0f433b9fcebec
Max Orelus <mo@counterflowai.com>
2018-12-14 11:12:23 -0500

    analytic fields updated

--- webapp/src/analytics.conf.js
+++ webapp/src/analytics.conf.js
@@ -5,5 +5,21 @@ export default {
     "triage.alert_count": "Alert Count",
     "triage.alerts_per_target": "Alerts per Target",
     "triage.priority": "Priority",
-    "triage.unique_targets": "Unique Targets"
+    "triage.unique_targets": "Unique Targets",
+    "bytes_mad.deviation": "Bytes Mad Deviation",
+    "bytes_mad.median": "Bytes Mad Median",
+    "bytes_mad.median_deviation": "Bytes Mad Median Deviation",
+    "bytes_mad.multiple": "Bytes Mad Multiple",
+    "bytes_mad.n": "Bytes Mad N",
+    "bytes_mad.since": "Bytes Mad Since",
+    "bytes_mad.source": "Bytes Mad Source",
+    "bytes_rank.dest_rank": "Bytes Destination Rank",
+    "bytes_rank.dest_total": "Bytes Destination Total",
+    "bytes_rank.since": "Bytes Rank Since",
+    "bytes_rank.source": "Bytes Rank Source",
+    "bytes_rank.src_rank": "Bytes Source Rank",
+    "bytes_rank.src_total": "Bytes Source Total",
+    "unique_src.count": "Unique Source Count",
+    "unique_src.since": "Unique Source Since",
+    "unique_src.source": "Unique Source"
 }
